ccc1bb32572d505a5b3d3c737d5dadd3dd0f36b3
S.K. Law <sk.law.lsq@gmail.com>
2020-02-03 13:15:14 -0500

    Update time.h

--- time.h
+++ time.h
@@ -1,7 +1,24 @@
 
+/*
+  \return timer reading in microsecond
+*/
 extern unsigned long now_usec ( void );
+
+/*
+  \param usecs time to wait in microsecond
+*/
 extern void wait( unsigned int usecs );
+
+/*
+  \param operand_1 time in microsecond
+  \param operand_2 another time in microsecond
+  \return time difference between operand_1 and operand_2 in microsecond
+*/
 unsigned long usec_diff ( unsigned long operand_1, unsigned long operand_2);
+
+/*
+  Initialize the timer you want to use for this project
+*/
 extern void init_timer();
 
 #define ONE_USEC	0x1
