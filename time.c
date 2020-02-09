2d2420dde5560840f6d423d0166e84407da4af5a
gcampisciano <42872053+gcampisciano@users.noreply.github.com>
2020-02-09 16:47:39 -0500

    Update time.c

--- time.c
+++ time.c
@@ -3,17 +3,27 @@
 
 unsigned long now_usec ( void )
 {
-	// Needs implementation
+	time_t seconds;
+	seconds = time(NULL);
+	return seconds;
 }
 
 unsigned long usec_diff ( unsigned long operand_1, unsigned long operand_2)
 {
-	// Needs implementation
+	time_t diff;
+	diff = difftime(operand_1, operand_2);
+	return diff;
 }
 
 void wait( unsigned int usecs )
 {
-	usleep(usecs);	
+	time_t start;
+	time_t diff;
+	start = now_usec();
+	diff = usec_diff(start, now_usec());
+	while(diff < usecs) {
+		diff = usec_diff(start, now_usec());
+	}
 }
 
 void init_timer()
